35fcc3b96c3149ae2a3e618c9b7a6e6b956cdce3
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-22 01:15:40 -0700

    Add some react!

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
-module.exports = {
+var bower_dir = __dirname + "/bower_components"
+var config = {
+  addVendor: function (name, path) {
+    this.resolve.alias[name] = path;
+  },
+
   entry: [ './app/main.js' ],
+  resolve: {
+    alias: {}
+  },
   output: {
     path: './build',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'jsx-loader' }
+    ]
   }
 };
+
+config.addVendor("react", bower_dir + "/react/react.min.js");
+
+module.exports = config;
